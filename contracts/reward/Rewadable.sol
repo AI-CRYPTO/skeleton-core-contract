6844e75f80895260407cd5ff6401d9ab402a1c39
JadyAiCrypto <jdlee@aicrypto.ai>
2018-07-27 17:24:43 +0900

    Initiation

--- contracts/reward/Rewadable.sol
+++ contracts/reward/Rewadable.sol
@@ -4,8 +4,11 @@ import "./Evaluable.sol";
 
 contract Rewadable is Evaluable {
 
-    function _readjust()
-        internal
-    {
-    }
+  function _readjust()
+    internal
+  {
+    /**
+    Evaluable usability from society
+    */
+  }
 }
